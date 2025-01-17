9c8b3e57375b9d6310aefd18ec8d79067a535aa6
Achiya Zigler <achiyazigi@gmail.com>
2025-01-17 02:47:19 +0200

    Refactor old code and revive the old model
    I added a patch after loading the pkl file.
    Basicaly i just hard code the missing attr:
            alephbert.bert.encoder.gradient_checkpointing = False

--- AlephBERT-main/models/alephbert-base/english-model.py
+++ AlephBERT-main/models/alephbert-base/english-model.py
@@ -3,66 +3,39 @@ import sys
 from transformers import BertTokenizerFast, BertForMaskedLM
 import json
 
-# OPTIONAL: if you want to have more information on what's happening, activate the logger as follows
-import logging
-logging.basicConfig(level=logging.INFO)
-
-# Load pre-trained model tokenizer (vocabulary)
+# Load pre-trained model tokenizer
 tokenizer = BertTokenizerFast.from_pretrained('bert-base-uncased')
 
+# Input sentence and mask
 sentence = sys.argv[1]
 mask = sys.argv[2]
-#original = sys.argv[3]
 sentence = sentence.replace(mask, '[MASK]')
-# print(sentence)
-text = '[CLS] ' + sentence + ' . [SEP]'
-tokenized_text = tokenizer.tokenize(text)
-indexed_tokens = tokenizer.convert_tokens_to_ids(tokenized_text)
 
-# Create the segments tensors.
-segments_ids = [0] * len(tokenized_text)
+# Tokenize the sentence
+inputs = tokenizer(sentence, return_tensors='pt', add_special_tokens=True)
+tokens_tensor = inputs['input_ids']
+segments_tensors = inputs['token_type_ids']
 
-# Convert inputs to PyTorch tensors
-tokens_tensor = torch.tensor([indexed_tokens])
-segments_tensors = torch.tensor([segments_ids])
-
-# Load pre-trained model (weights)
+# Load pre-trained model
 model = BertForMaskedLM.from_pretrained('bert-base-uncased')
 model.eval()
 
 # Predict all tokens
 with torch.no_grad():
-    predictions = model(tokens_tensor, segments_tensors)
+    predictions = model(tokens_tensor, token_type_ids=segments_tensors)
 
-masked_index = tokenized_text.index('[MASK]')
+# Get the index of the masked token
+masked_index = (tokens_tensor[0] == tokenizer.mask_token_id).nonzero(as_tuple=True)[0].item()
+
+# Get top predictions
 num_of_res = 10
+logits = predictions.logits[0, masked_index]
+probs = torch.nn.functional.softmax(logits, dim=0)
 
-predicted_sorted = torch.argsort(
-    predictions[0][0, masked_index], descending=True)
-topres = tokenizer.convert_ids_to_tokens(
-    [x.item() for x in predicted_sorted[:num_of_res]])
+top_indices = torch.argsort(probs, descending=True)[:num_of_res]
+top_tokens = tokenizer.convert_ids_to_tokens(top_indices.tolist())
+top_probs = probs[top_indices]
 
-
-def probs_from_predictions(predictions):
-    min_prediction = -predictions[0][0, masked_index][predicted_sorted[-1]]
-    sum_of_predictions = torch.sum(predictions[0][0, masked_index])
-    return (predictions[0][0, masked_index] + min_prediction) / (sum_of_predictions + min_prediction * len(predicted_sorted))
-
-
-probs = probs_from_predictions(predictions)
-
-sum_of_probs = torch.sum(probs)
-assert(abs(sum_of_probs - 1) < 0.00001)
-
-top_probs = torch.tensor(
-    [probs[tokenizer.convert_tokens_to_ids([res])[0]].item() for res in topres])
-sum_of_top = torch.sum(top_probs)
-
-relative_probs = top_probs / sum_of_top
-# nice_probs = [('{:.5f}'.format(x.item())) for x in relative_probs]
-
-with_prob = list(zip(topres, relative_probs.tolist()))
-# as_dicts = []
-# for (w,p) in with_prob:
-#     as_dicts.append({'word': w, 'prob': p})
+# Combine tokens with their probabilities
+with_prob = list(zip(top_tokens, top_probs.tolist()))
 print(json.dumps(with_prob))
--- AlephBERT-main/models/alephbert-base/hebrew-model.py
+++ AlephBERT-main/models/alephbert-base/hebrew-model.py
@@ -8,6 +8,7 @@ import sys
 torch.device('cpu')
 
 
+# Custom Unpickler to load a model on CPU
 class CPU_Unpickler(pickle.Unpickler):
     def find_class(self, module, name):
         if module == 'torch.storage' and name == '_load_from_bytes':
@@ -16,71 +17,49 @@ class CPU_Unpickler(pickle.Unpickler):
             return super().find_class(module, name)
 
 
+# Inputs
 text = sys.argv[1]
 mask = sys.argv[2]
 original = sys.argv[3]
 
-alephbert_tokenizer = BertTokenizerFast.from_pretrained(
-    'onlplab/alephbert-base')
+# Load tokenizer
+alephbert_tokenizer = BertTokenizerFast.from_pretrained('onlplab/alephbert-base')
 
+# Load model (pretrained or custom)
 if original == "true":
     alephbert = BertForMaskedLM.from_pretrained('onlplab/alephbert-base')
 else:
-    alephbert = CPU_Unpickler(
-        open('../../AlephBERT-main/models/myFirstTune/tune_6_10.pkl', 'rb')).load()
+    with open('../../AlephBERT-main/models/myFirstTune/tune_6_10.pkl', 'rb') as model_file:
+        alephbert = CPU_Unpickler(model_file).load()
+        alephbert.bert.encoder.gradient_checkpointing = False
 
-# alephbert = CPU_Unpickler(open('../../models/myFirstTune/tune_2.pkl', 'rb')).load()
-
-#print("after alephbert model loading")
-
-text = text.replace(mask, '[MASK]')
-hebrew_text = '[CLS] ' + text + ' . [SEP]'
-
-tokenized_text = alephbert_tokenizer.tokenize(hebrew_text)
-indexed_tokens = alephbert_tokenizer.convert_tokens_to_ids(tokenized_text)
-
-
-# Create the segments tensors.
-segments_ids = [0] * len(tokenized_text)
-
-# Convert inputs to PyTorch tensors
-tokens_tensor = torch.tensor([indexed_tokens])
-
-segments_tensors = torch.tensor([segments_ids])
-
-# Load pre-trained model (weights)
 alephbert.eval()
 
-# Predict all tokens
+# Prepare the text with [MASK] token
+hebrew_text = text.replace(mask, '[MASK]')
+inputs = alephbert_tokenizer(hebrew_text, return_tensors='pt', add_special_tokens=True)
+
+tokens_tensor = inputs['input_ids']
+segments_tensors = inputs['token_type_ids']
+
+# Predict masked tokens
 with torch.no_grad():
-    predictions = alephbert(tokens_tensor, segments_tensors)
+    predictions = alephbert(tokens_tensor, token_type_ids=segments_tensors)
 
-masked_index = tokenized_text.index('[MASK]')
+# Locate the [MASK] token index
+masked_index = (tokens_tensor[0] == alephbert_tokenizer.mask_token_id).nonzero(as_tuple=True)[0].item()
+
+# Get the top predictions and probabilities
 num_of_res = 10
+logits = predictions.logits[0, masked_index]
+probs = torch.nn.functional.softmax(logits, dim=0)
 
-predicted_sorted = torch.argsort(
-    predictions[0][0, masked_index], descending=True)
-topres = alephbert_tokenizer.convert_ids_to_tokens(
-    [x.item() for x in predicted_sorted[:num_of_res]])
+top_indices = torch.argsort(probs, descending=True)[:num_of_res]
+top_tokens = alephbert_tokenizer.convert_ids_to_tokens(top_indices.tolist())
+top_probs = probs[top_indices]
 
+# Combine tokens with their probabilities
+with_prob = list(zip(top_tokens, top_probs.tolist()))
 
-def probs_from_predictions(predictions):
-    min_prediction = -predictions[0][0, masked_index][predicted_sorted[-1]]
-    sum_of_predictions = torch.sum(predictions[0][0, masked_index])
-    return (predictions[0][0, masked_index] + min_prediction) / (sum_of_predictions + min_prediction * len(predicted_sorted))
-
-
-probs = probs_from_predictions(predictions)
-
-sum_of_probs = torch.sum(probs)
-assert(abs(sum_of_probs - 1) < 0.00001)
-
-top_probs = torch.tensor(
-    [probs[alephbert_tokenizer.convert_tokens_to_ids([res])[0]].item() for res in topres])
-sum_of_top = torch.sum(top_probs)
-
-relative_probs = top_probs / sum_of_top
-
-with_prob = list(zip(topres, relative_probs.tolist()))
-
+# Output as JSON
 print(json.dumps(with_prob))
